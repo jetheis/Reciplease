646d0bd5549ec95b0500a7a431790885952a1618
Rob <robwagner33@gmail.com>
2012-12-13 14:59:19 -0500

    Removed gravatar for now

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,13 +11,4 @@ module ApplicationHelper
     HTML
     html.html_safe
   end
-  
-  def gravatar_for email, options = {}
-    options = {:alt => 'avatar', :class => 'avatar', :size => 80}.merge! options
-    id = Digest::MD5::hexdigest email.strip.downcase
-    url = 'http://www.gravatar.com/avatar/' + id + '.jpg?s=' + options[:size].to_s
-    options.delete :size
-    image_tag url, options
-  end
-  
 end
