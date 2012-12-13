9efffd5db92d2fce4a4d9e5724dd2a694f591713
Rob <robwagner33@gmail.com>
2012-12-12 22:24:23 -0500

    Authentication Setup

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ActiveRecord::Base
+  rolify
+  
+  validates_presence_of :name
+  validates_uniqueness_of :name, :email, :case_sensitive => false
+  
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :name, :password, :password_confirmation, :remember_me
+end
