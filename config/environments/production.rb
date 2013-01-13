bf3b2cbe5bc1ceaccc98d5e006088f08f78e8fa7
Rob <robwagner33@gmail.com>
2013-01-13 14:56:40 -0500

    Changed domain to reciplease-test for testing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,9 +48,6 @@ Reciplease::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
 
-  # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
-
   # Enable threaded mode
   # config.threadsafe!
 
@@ -69,13 +66,14 @@ Reciplease::Application.configure do
     :enable_starttls_auto => true,     
     :address            => 'smtp.gmail.com',   
     :port               => 587,    
-    :domain             => 'reciplease.herokuapp.com',    
+    :domain             => 'reciplease-test.herokuapp.com',    
     :authentication     => :plain,   
     :user_name          => ENV['EMAIL_SERVER_USER'],   
     :password           => ENV['EMAIL_SERVER_PASS']
   }   
     
- config.action_mailer.default_url_options = { :host => 'reciplease.herokuapp.com' }   
+ #TODO: Update this to reciplease, instead of reciplease-test
+ config.action_mailer.default_url_options = { :host => 'reciplease-test.herokuapp.com' }   
   
 
   # Log the query plan for queries taking more than this (works
