33485fdc770bcd8aec96e27ef08382433248001f
Robbie Cooper <cooperra@rose-hulman.edu>
2013-01-24 17:00:56 -0500

    Implement editing. Add auth checks to recipe methods.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -27,27 +27,49 @@ class RecipesController < ApplicationController
     @recipe = Recipe.new
     #@ingredients = [Ingredient.new, Ingredient.new]
 
+    is_logged_in = current_user != nil
+    
     respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @recipe }
+      if is_logged_in
+        format.html # new.html.erb
+        format.json { render json: @recipe }
+      else
+        format.html { redirect_to recipes_url, alert: "You need to login to create recipes." }
+        format.json { head :no_content, status: :unauthorized }
+      end
     end
   end
 
   # GET /recipes/1/edit
   def edit
     @recipe = Recipe.find(params[:id])
+
+    is_logged_in = current_user != nil
+    is_owner = @recipe.owner == current_user
+    
+    if not is_logged_in
+      redirect_to @recipe, alert: "You need to login to edit recipes."
+    elsif not is_owner
+      redirect_to @recipe, alert: "You do not have permission to edit this recipe."
+    end
   end
 
   # POST /recipes
   # POST /recipes.json
   def create
-    @recipe_fields = params[:recipe]
-    @recipe_fields[:owner_id] = current_user.id
+    is_logged_in = current_user != nil
 
+    @recipe_fields = params[:recipe]
+    if is_logged_in
+      @recipe_fields[:owner_id] = current_user.id
+    end
     @recipe = Recipe.new(@recipe_fields)
 
     respond_to do |format|
-      if @recipe.save
+      if not is_logged_in
+        format.html { render action: "new", alert: "You need to login to create recipes.", status: :unauthorized }
+        format.json { render json: @recipe.errors, status: :unauthorized }
+      elsif @recipe.save
         format.html { redirect_to @recipe, notice: 'Recipe was successfully created.' }
         format.json { render json: @recipe, status: :created, location: @recipe }
       else
@@ -61,9 +83,17 @@ class RecipesController < ApplicationController
   # PUT /recipes/1.json
   def update
     @recipe = Recipe.find(params[:id])
+    is_logged_in = current_user != nil
+    is_owner = @recipe.owner == current_user
 
     respond_to do |format|
-      if @recipe.update_attributes(params[:recipe])
+      if not is_logged_in
+        format.html { render action: "show", alert: "You need to login to edit recipes.", status: :unauthorized }
+        format.json { render json: @recipe.errors, status: :unauthorized }
+      elsif not is_owner
+        format.html { render action: "show", alert: "You do not have permission to edit this recipe.", status: :forbidden }
+        format.json { render json: @recipe.errors, status: :forbidden }
+      elsif @recipe.update_attributes(params[:recipe])
         format.html { redirect_to @recipe, notice: 'Recipe was successfully updated.' }
         format.json { head :no_content }
       else
@@ -77,11 +107,26 @@ class RecipesController < ApplicationController
   # DELETE /recipes/1.json
   def destroy
     @recipe = Recipe.find(params[:id])
-    @recipe.destroy
+    
+    is_logged_in = current_user != nil
+    is_owner = @recipe.owner == current_user
 
+    if is_logged_in and is_owner
+      @recipe.destroy
+    end
+    
     respond_to do |format|
-      format.html { redirect_to recipes_url }
-      format.json { head :no_content }
+      if not is_logged_in
+        format.html { redirect_to @recipe, alert: "You need to login to delete recipes." }
+        format.json { head :no_content, status: :unauthorized }
+      elsif not is_owner
+        format.html { redirect_to @recipe, alert: "You do not have permission to delete this recipe." }
+        format.json { head :no_content, status: :forbidden }
+      else
+        # Success
+        format.html { redirect_to recipes_url }
+        format.json { head :no_content }
+      end
     end
   end
 end
