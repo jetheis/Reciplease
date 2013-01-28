8c8194f946f76302c74811382a6dfbeb06a8c343
Alex Petitjean <petitjam@rose-hulman.edu>
2013-01-28 16:03:09 -0500

    removed some of the dynamic ingredients form code

--- app/assets/javascripts/dynamic_ingredients_form_field.js
+++ app/assets/javascripts/dynamic_ingredients_form_field.js
@@ -1,13 +1,12 @@
 $(document).ready(function() {
   $('a#add-another').click(function() {
-    $('#ingredients-list li:first').clone().find('input').val('')
-    .end().appendTo('#ingredients-list');
+    $('#ingredients-list li:first').clone().find('input').val('').end().appendTo('#ingredients-list');
   });
 
   $('.delete-ingredient').live('click', function() {
     if ($('#ingredients-list li').length > 1)
-  $(this).parent().remove();
+      $(this).parent().remove();
     else
-  alert('You need at least one ingredient.')
+      alert('You need at least one ingredient.')
   });
-});
+}); 
