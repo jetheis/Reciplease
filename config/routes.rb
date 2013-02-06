7553b94d8ec1beeaf21a98dfd793ddd5795b0cad
Alex Petitjean <petitjam@rose-hulman.edu>
2013-02-06 16:25:35 -0500

    removing leftover opinio

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Reciplease::Application.routes.draw do
-  opinio_model
-
   resources :recipes
-  resources :recipes do
-    opinio
-  end
   
   authenticated :user do
     root :to => 'home#home'
