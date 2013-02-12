a959403c2d1581dbb7a11c36e56575b727ac3956
Rob <robwagner33@gmail.com>
2013-02-12 14:42:53 -0500

    Reoganized recipe page for mobile support, added igredients support

--- db/migrate/20130113183207_add_recipe_ingredient.rb
+++ db/migrate/20130113183207_add_recipe_ingredient.rb
@@ -2,6 +2,8 @@ class AddRecipeIngredient < ActiveRecord::Migration
   def change
     create_table(:recipes) do |t|
       t.string :name
+      # TODO: This will be removed upon fully implementing Ingredients
+      t.string :ingredients
       t.string :description
       t.string :instructions
       t.references :resource, :polymorphic => true
