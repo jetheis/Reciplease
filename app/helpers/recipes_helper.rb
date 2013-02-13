73d580974dfe4124a82eaacdf4c1ffdc0856f089
Robbie Cooper <cooperra@rose-hulman.edu>
2013-02-13 01:41:24 -0500

    Fix not-logged-in issue caused by ratings

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -3,15 +3,19 @@ module RecipesHelper
   # Return's the current user's rating object for the current recipe
   # Or returns a new rating object
   def rating_ballot
-    if @rating = current_user.ratings.find_by_recipe_id(params[:id])
-      @rating
-    else
-      current_user.ratings.new
+    if user_signed_in?
+      if @rating = current_user.ratings.find_by_recipe_id(params[:id])
+        @rating
+      else
+        current_user.ratings.new
+      end
+    else # not signed in
+      Rating.new
     end
   end
 
   def current_user_rating
-    if @rating = current_user.ratings.find_by_recipe_id(params[:id])
+    if user_signed_in? and @rating = current_user.ratings.find_by_recipe_id(params[:id])
       @rating.score
     else
       "N/A"
