2d374b3b9a6303f29a7a83da4ab291530915717f
Rob <robwagner33@gmail.com>
2013-03-12 19:45:59 -0400

    Added all users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
     
   def show
     @user = User.find(params[:id])
+    @users = User.all
     @user_recipes = Recipe.where(:owner_id => @user)
     @fav_recipes = Recipe.where(:id => Fav_Recipe.where(:user_id => @user).select("recipe_id"))
     respond_to do |format|
