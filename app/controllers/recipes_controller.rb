e08e7ab49deee9738cd379ea496dc8a6ea9fb2b0
muglump <korahel@gmail.com>
2013-04-14 15:56:11 -0400

    So I think I go rid of what I could but we need what is left as far as I can tell.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,12 +1,9 @@
 class RecipesController < ApplicationController
-  #TODO: BAD SMELL-DAVID A lot of the format blocks can be removed
-  
   # GET /recipes
   # GET /recipes.json
   def index
     @recipes = Recipe.all
     @top_recipes = nil
-
     respond_to do |format|
       format.html # index.html.erb
       format.json do
@@ -19,7 +16,6 @@ class RecipesController < ApplicationController
   # GET /recipes/1.json
   def show
     @recipe = Recipe.find(params[:id])
-
     respond_to do |format|
       format.html # show.html.erb
       format.json do
@@ -32,10 +28,7 @@ class RecipesController < ApplicationController
   # GET /recipes/new.json
   def new
     @recipe = Recipe.new
-    #@ingredients = [Ingredient.new, Ingredient.new]
-
     is_logged_in = current_user != nil
-    
     respond_to do |format|
       if is_logged_in
         format.html # new.html.erb
@@ -48,10 +41,8 @@ class RecipesController < ApplicationController
   # GET /recipes/1/edit
   def edit
     @recipe = Recipe.find(params[:id])
-
     is_logged_in = current_user != nil
     is_owner = @recipe.owner == current_user
-    
     if not is_logged_in
       redirect_to @recipe, alert: "You need to login to edit recipes."
     elsif not is_owner
@@ -63,21 +54,17 @@ class RecipesController < ApplicationController
   def personalize
     @recipe = Recipe.find(params[:id])
     @recipe.parent_id = params[:id];
-
     is_logged_in = current_user != nil
     is_owner = Recipe.find(params[:id]).owner == current_user
-    
     respond_to do |format|
       if is_logged_in
         format.html # personalize.html.erb
       else
         format.html { redirect_to recipe_path(@recipe.parent_id), alert: "You need to login to personalize recipes." }
       end
-      
       if is_owner
         format.html { redirect_to edit_recipe_path }
       end
-
     end
   end
 
@@ -85,13 +72,11 @@ class RecipesController < ApplicationController
   # POST /recipes.json 
   def create
     is_logged_in = current_user != nil
-
     @recipe_fields = params[:recipe]
     if is_logged_in
       @recipe_fields[:owner_id] = current_user.id
     end
     @recipe = Recipe.new(@recipe_fields)
-
     respond_to do |format|
       if not is_logged_in
         format.html { render action: "new", alert: "You need to login to create recipes.", status: :unauthorized }
@@ -112,7 +97,6 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find(params[:id])
     is_logged_in = current_user != nil
     is_owner = @recipe.owner == current_user
-
     respond_to do |format|
       if not is_logged_in
         format.html { render action: "show", alert: "You need to login to edit recipes.", status: :unauthorized }
@@ -133,15 +117,12 @@ class RecipesController < ApplicationController
   # DELETE /recipes/1
   # DELETE /recipes/1.json
   def destroy
-    @recipe = Recipe.find(params[:id])
-    
+    @recipe = Recipe.find(params[:id])   
     is_logged_in = current_user != nil
     is_owner = @recipe.owner == current_user
-
     if is_logged_in and is_owner
       @recipe.destroy
-    end
-    
+    end  
     respond_to do |format|
       if not is_logged_in
         format.html { redirect_to @recipe, alert: "You need to login to delete recipes." }
@@ -156,5 +137,4 @@ class RecipesController < ApplicationController
       end
     end
   end
-
 end
